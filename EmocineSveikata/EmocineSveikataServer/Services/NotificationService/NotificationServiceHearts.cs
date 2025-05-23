7fd0164b5cb3e86a9cc5ef9534aa133c5ecf87f3
Dominykas Dobkevičius <144054096+DominykasDo@users.noreply.github.com>
2025-05-23 17:54:12 +0300

    NotificationService with Strategy Design Pattern

---
+++ EmocineSveikata/EmocineSveikataServer/Services/NotificationService/NotificationServiceHearts.cs
@@ -0,0 +1,39 @@
+﻿using EmocineSveikataServer.Dto;
+
+namespace EmocineSveikataServer.Services.NotificationService
+{
+    public class NotificationServiceHearts : INotificationService
+    {
+        private readonly NotificationService _originalNotificationService;
+
+        public NotificationServiceHearts(NotificationService originalNotificationService)
+        {
+            _originalNotificationService = originalNotificationService;
+        }
+
+        public async Task CreateNotificationAsync(string message, int userId, string link = "")
+        {
+            await _originalNotificationService.CreateNotificationAsync(message, userId, link);
+        }
+
+        public async Task<List<NotificationDto>?> GetNotificationsAsync(int userId)
+        {
+            var notificationDtos = await _originalNotificationService.GetNotificationsAsync(userId);
+
+            if(notificationDtos != null)
+            {
+                foreach(var notification in notificationDtos)
+                {
+                    notification.Message = $"❤️{notification.Message}❤️";
+                }
+            }
+
+            return notificationDtos;
+        }
+
+        public async Task MarkAllAsReadAsync(int userId)
+        {
+            await _originalNotificationService.MarkAllAsReadAsync(userId);
+        }
+    }
+}
